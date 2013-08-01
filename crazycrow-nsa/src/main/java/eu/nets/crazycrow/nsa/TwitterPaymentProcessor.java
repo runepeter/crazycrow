512feb8fc0316a6086eb7521f2a143ebdada317e
Steingrim Dovland <sdovl@nets.eu>
2013-08-01 14:28:03 +0200

    Work in progress..

--- crazycrow-nsa/src/main/java/eu/nets/crazycrow/nsa/TwitterPaymentProcessor.java
+++ crazycrow-nsa/src/main/java/eu/nets/crazycrow/nsa/TwitterPaymentProcessor.java
@@ -1,5 +1,8 @@
 package eu.nets.crazycrow.nsa;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.slf4j.Logger;
@@ -9,7 +12,7 @@ import twitter4j.Status;
 
 final public class TwitterPaymentProcessor implements Processor {
 	
-	private final Logger logger = LoggerFactory.getLogger(TwitterPaymentProcessor.class);
+	private static final Logger logger = LoggerFactory.getLogger(TwitterPaymentProcessor.class);
 	
 	@Override
 	public void process(Exchange exchange) throws Exception {
@@ -23,6 +26,16 @@ final public class TwitterPaymentProcessor implements Processor {
 		paymentInstruction.setDebitSocialId(status.getUser().getName());
 		
 		String text = status.getText();
+
+		Pattern pattern = Pattern.compile("^[@](\\s+) Her får du (\\d+) kroner.*$");
+		Matcher matcher = pattern.matcher(text);
+		
+		if (matcher.matches()) {
+			String receiver = matcher.group(1);
+			String amount = matcher.group(2);
+		} else {
+			throw new IllegalArgumentException("Could not parse text: " + text);
+		}
 		
 		return paymentInstruction;
 	}
