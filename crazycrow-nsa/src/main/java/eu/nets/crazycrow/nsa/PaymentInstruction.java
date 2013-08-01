eeb6f25e780b6703d54dd2d73bee57492af53392
Steingrim Dovland <sdovl@nets.eu>
2013-08-01 13:51:05 +0200

    Consume payments from file

---
+++ crazycrow-nsa/src/main/java/eu/nets/crazycrow/nsa/PaymentInstruction.java
@@ -0,0 +1,79 @@
+package eu.nets.crazycrow.nsa;
+
+import java.math.BigDecimal;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class PaymentInstruction {
+
+	private Long id;
+	private Source source;
+	private BigDecimal amount;
+	private String debitSocialId;
+	private String creditSocialId;
+	private String debitAccount;
+	private String creditAccount;
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Source getSource() {
+		return source;
+	}
+
+	public void setSource(Source source) {
+		this.source = source;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public String getDebitSocialId() {
+		return debitSocialId;
+	}
+
+	public void setDebitSocialId(String debitSocialId) {
+		this.debitSocialId = debitSocialId;
+	}
+
+	public String getCreditSocialId() {
+		return creditSocialId;
+	}
+
+	public void setCreditSocialId(String creditSocialId) {
+		this.creditSocialId = creditSocialId;
+	}
+
+	public String getDebitAccount() {
+		return debitAccount;
+	}
+
+	public void setDebitAccount(String debitAccount) {
+		this.debitAccount = debitAccount;
+	}
+
+	public String getCreditAccount() {
+		return creditAccount;
+	}
+
+	public void setCreditAccount(String creditAccount) {
+		this.creditAccount = creditAccount;
+	}
+
+}
